d1a53dfcb078c62a3a396f36941d5fb183e330c9
Emerson Valentim <emevalentim0@gmail.com>
2022-06-05 17:58:47 -0300

    feat: add us east certificate

--- applications/dns/main.tf
+++ applications/dns/main.tf
@@ -1,5 +1,6 @@
 locals {
   zone_id         = "Z03308441EM1ONQR0GRUX"
   certificate_arn = "arn:aws:acm:sa-east-1:567645314860:certificate/9d8bb923-4c01-41e7-b1db-de6e4e754763"
+  certificate_arn_us_east = "arn:aws:acm:us-east-1:567645314860:certificate/652965b0-c0ac-4cea-92f4-5b189119b739"
   domain          = "evshosting.net"
 }
--- applications/dns/output.tf
+++ applications/dns/output.tf
@@ -6,6 +6,9 @@ output "certificate_arn" {
   value = local.certificate_arn
 }
 
+output "certificate_arn_us_east" {
+  value = local.certificate_arn_us_east
+}
 output "domain" {
   value = local.domain
 }
